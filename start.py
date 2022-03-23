826f32709fd15c01cd334b2178cbee08c1ad95a3
R-Lovelett <83929892+R-Lovelett@users.noreply.github.com>
2022-03-23 17:32:55 -0500

    Comments

--- start.py
+++ start.py
@@ -1,48 +1,53 @@
 def newGame():
-	print("Hello! Welcome to Madlibs!\n")
+    print("Hello! Welcome to Madlibs!\n")
 
-	while True: # Until player enters number for story selection, keep asking player to select story
-		try:
-			print("1. Short story")
-			print("2. Medium story\n")
-			storySelect = int(input("Select a story that you would like to use:"))
-		except ValueError: # If somehow the input isnt recognized, loop again
-			print("Sorry, I didn't understand that. Please try again.")
-			continue
+    # Until player enters  1 or 2, keep asking player to select story
+    while True:
+        try:
+            print("1. Short story")
+            print("2. Medium story\n")
+            storySelect = int(input("Select a story that you would like to use:"))
+        except ValueError: # If somehow the input isnt recognized, loop again
+            print("Sorry, I didn't understand that. Please try again.")
+            continue
 
-		if storySelect == 1:
-			shortStory()
-			break
-		elif storySelect == 2:
-			medStory()
-			break
+        #Select corresponding story based on user input
+        if storySelect == 1:
+            shortStory()
+            break
+        elif storySelect == 2:
+            medStory()
+            break
 
 def shortStory():
-	noun_List = {}
-	adj_List = {}
-	verb_List = {}
-	x = 1
+    noun_List = {}
+    adj_List = {}
+    verb_List = {}
+    x = 1
 
-	print("Time for a short story!\nPlease answer the following "
-		"prompts in order to complete the story!")
+    print("Time for a short story!\nPlease answer the following "
+        "prompts in order to complete the story!")
 
-	while x < 10:
-		if x <= 3:
-			noun_List[x] = input("Enter a noun: ")
-		if x <= 6 and x > 3:
-			adj_List[x] = input("Enter a adjective: ")
-		if x <= 9 and x > 6:
-			verb_List[x] = input("Enter a verb: ")
-		x += 1
+    # Ask user for 3 of each category: nouns, adjectives, and verbs
+    while x < 10:
+        if x <= 3:
+            noun_List[x] = input("Enter a noun: ")
+        if x <= 6 and x > 3:
+            adj_List[x] = input("Enter a adjective: ")
+        if x <= 9 and x > 6:
+            verb_List[x] = input("Enter a verb: ")
+        x += 1
 
-	print(f"\nOur school cafeteria has really {adj_List[4]} food.")
-	print(f"Just thinking about it makes my stomach {verb_List[7]}. The spaghetti ")
-	print(f"is {adj_List[5]} and tastes like {noun_List[1]}. One day, I swear the meatballs")
-	print(f"started to {verb_List[8]}! The turkey tacos are totally {adj_List[6]} and kind")
-	print(f"of look like old {noun_List[2]}. Don't get me started on the hoagies, I")
-	print(f"would rather {verb_List[9]} to {noun_List[3]} before taking a bite of one!")
+    #Printed out final story
+    print(f"\nOur school cafeteria has really {adj_List[4]} food.")
+    print(f"Just thinking about it makes my stomach {verb_List[7]}. The spaghetti ")
+    print(f"is {adj_List[5]} and tastes like {noun_List[1]}. One day, I swear the meatballs")
+    print(f"started to {verb_List[8]}! The turkey tacos are totally {adj_List[6]} and kind")
+    print(f"of look like old {noun_List[2]}. Don't get me started on the hoagies, I")
+    print(f"would rather {verb_List[9]} to {noun_List[3]} before taking a bite of one!")
 
 def medStory():
-	print("Medium story here.")
+    print("Medium story here.")
+    # TODO add story here
 
 newGame()
